222dd5567429017b3825ed83349218348176858a
mebin-rahman <mebinfateha@gmail.com>
2021-12-07 13:40:15 +0600

    header added in musician's profile

---
+++ mprofile.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mprofile.css?v=<?php echo time();?>">
+ 
+   <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">  -->
+
+  <title>Musiconnect</title>
+  
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero"><h1><span>MUSI</span>CON<span>N</span>ECT</h1></a>
+        </div>
+        <div class="nav-list">
+          <!-- <div class="hamburger"><div class="bar"></div></div> -->
+          <ul>
+            <li><a href="#" data-after="Home">HOME</a></li>
+            <li><a href="musicianprofileedit.php" data-after="Service">EDIT PROFILE</a></li>
+            <!-- <li><a href="#" data-after="Projects">REGISTRATION</a></li> -->
+            <li><a href="#" data-after="Contact">Log Out</a></li>
+            
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  
+</body>
+</html>
