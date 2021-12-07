654108498fee19de03e63bb0726da918dd44cd6b
DiptoDey321 <diptodey2005@gmail.com>
2021-12-08 00:52:00 +0600

    database file added

---
+++ music.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE if not exists music;
+use music;
+
+CREATE table booking (
+    id int(10) AUTO_INCREMENT UNIQUE,
+    event varchar(20) NOT NULL,
+    date date NOT NULL,
+    place varchar(100) NOT NULL,
+    email varchar(30) NOT NULL,
+    number int(20) NOT NULL,
+    
+    PRIMARY KEY(id) 
+);
+
