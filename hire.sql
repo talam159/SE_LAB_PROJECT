25297b724986c72a51f55f16c43140e9291d4900
monir102 <mdmonirozzaman7@gmail.com>
2021-12-08 12:44:20 +0600

    database added

---
+++ hire.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE if not exists connects;
+use connects;
+
+CREATE table booking (
+    id int(10) AUTO_INCREMENT UNIQUE,
+    office_name varchar(20) NOT NULL,
+    start_date date NOT NULL,
+    place varchar(100) NOT NULL,
+    email varchar(30) NOT NULL,
+    number int(20) NOT NULL,
+    
+    PRIMARY KEY(id) 
+);
+
