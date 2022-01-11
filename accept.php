29cdfe76cd44a7ea9f38981f70cabec1b08d52df
DiptoDey321 <diptodey2005@gmail.com>
2022-01-11 23:17:42 +0600

    accept of the the audience reqst is added. its working now

---
+++ accept.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include('db_connect.php');
+
+
+$c="confirm";
+//$email= $_SESSION['email'];
+$id=$_GET['id'];
+$query= "UPDATE booking SET status='$c' WHERE id='$id' ";
+$data=mysqli_query($con,$query);
+if($data){
+	echo "Updated";
+}
+else{
+	echo "Failed to delete";
+}
+?>
