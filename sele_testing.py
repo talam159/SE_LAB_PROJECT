332e012e7f0cfab0f47dee6aebd358d213ce8ac4
Md Monirozzaman <94528047+Munir102@users.noreply.github.com>
2022-01-19 16:24:18 +0600

    Update sele_testing.py

--- sele_testing.py
+++ sele_testing.py
@@ -7,16 +7,4 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("http://localhost/hire/sele_frontend.html")
 
-inp = driver.find_element_by_id('inpt')
-submit = driver.find_element_by_id('b1')
 
-inp.send_keys('2')
-
-submit.click()
-
-time.sleep(5)
-
-print(driver.find_element_by_id('office').text)
-print(driver.find_element_by_id('location').text)
-
-x=input('')
